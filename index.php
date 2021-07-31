1361084b74c1c7b898117f36e40fda3ad3af8d3d
sidiqsanjaya <44673223+sidiqsanjaya@users.noreply.github.com>
2021-07-31 22:17:28 +0700

    update dashboard

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php  
-// session_start();
+ session_start();
+ require "includes/functions.php";
 // if(isset($_SESSION["id_Pegawai"])){
 //     header("Location: index-admin.php");
 // } else
 // session_destroy();
 ?>
 <?php
-// header('Location: ?page=home');
+
 if (isset($_GET['page'])) {
     $page = isset($_GET['page']) ? $_GET['page'] : "";
     $error = isset($_GET['page']) ? $_GET['page'] : "";
@@ -14,54 +15,73 @@ if (isset($_GET['page'])) {
     $id = isset($_GET['id']) ? $_GET['id'] : "";
     if ($page == 'aksilogin'||$error == 1||$error == 2||$error == 3 ||$error == 4) {
         require 'aksilogin.php';
+    
+    //index owner
     } elseif ($page == 'owner') {
         require 'owner/f200.php';
+    } elseif ($page == 'listpegawai') {
+        require 'owner/f201.php';
+    } elseif ($page == 'tambahpegawai') {
+        require 'owner/f202.php';
+    } elseif ($page == 'editpegawai') {
+        require 'owner/f203.php';
+    }elseif ($page == 'hapuspegawai') {
+        require 'owner/hapusdatapegawai.php';
+    } elseif ($page == 'validasimenu') {
+        require 'owner/f204.php';
+    }elseif ($page == 'hapusmenu') {
+        require 'owner/hapusdatamenu.php';
+    } elseif ($page == 'laporankeuangan') {
+        require 'owner/f205.php';
+    
+
+    //index kasir    
     } elseif ($page == 'kasir') {
         require 'kasir/f210.php';
+    } elseif ($page == 'listdatabayar') {
+        require 'kasir/f211.php';
+    } elseif ($page == 'pembayaran') {
+        require 'kasir/f212.php';
+    } elseif ($page == 'laporan') {
+        require 'kasir/f213.php';
+    } elseif ($page == 'detaillaporan') {
+        require 'kasir/f214.php';
+    
+
+    //index barista
     } elseif ($page == 'barista') {
         require 'barista/f220.php';
+    } elseif ($page == 'listmenub') {
+        require 'barista/f221.php';
+    } elseif ($page == 'tambahrekomendasi') {
+        require 'barista/f222.php';
+    } elseif ($page == 'listpemesanan') {
+        require 'barista/f223.php';
+          
+
+    //index pelayan
     } elseif ($page == 'pelayan') {
         require 'pelayan/f230.php';
-    // } elseif ($page == 'aksilogin'||$error == 1||$error == 2||$error == 3) {
-    //     require 'login/login.php';
-    // } elseif ($page == 'menu') {
-    //     require 'login/index-admin.php';
-    // } 
-    // elseif ($page == 'kategori-admin') {
-    //     require 'content/admin/kategori.php';
-    // } elseif ($page == 'kategori-tambah') {
-    //     require 'content/admin/kategori_simpan.php';
-    // } elseif ($page == 'kategori-hapus'&&$id!=null) {
-    //     require 'content/admin/kategori_hapus.php';
-    // } elseif ($page == 'kategori-update') {
-    //     require 'content/admin/kategori_update.php';
-    // } 
-    // elseif ($page == 'studio-admin') {
-    //     require 'content/admin/studio.php';
-    // } elseif ($page == 'studio-tambah') {
-    //     require 'content/admin/studio_simpan.php';
-    // } elseif ($page == 'studio-hapus'&&$id!=null) {
-    //     require 'content/admin/studio_hapus.php';
-    // } elseif ($page == 'studio-update') {
-    //     require 'content/admin/studio_update.php';
-    // } 
-    // elseif ($page == 'user') {
-    //     require 'content/admin/user.php';
-    // } elseif ($page == 'user-tambah') {
-    //     require 'content/admin/user_simpan.php';
-    // } elseif ($page == 'user-hapus'&&$id!=null) {
-    //     require 'content/admin/user_hapus.php';
-    // } elseif ($page == 'user-update') {
-    //     require 'content/admin/user_update.php';
-    // } 
-    // elseif ($page == 'film') {
-    //     require 'content/admin/film.php';
-    // } elseif ($page == 'film-tambah') {
-    //     require 'content/admin/film_simpan.php';
-    // } elseif ($page == 'film-hapus'&&$id!=null&&$gambar!=null) {
-    //     require 'content/admin/film_hapus.php';
-    // } elseif ($page == 'film-update') {
-    //     require 'content/admin/film_update.php';
+    } elseif ($page == 'listmenup') {
+        require 'pelayan/f231.php';
+    } elseif ($page == 'listmeja') {
+        require 'pelayan/f232.php';
+    } elseif ($page == 'pemesanan') {
+        require 'pelayan/f233.php';
+    } elseif ($page == 'tambahmeja') {
+        require 'pelayan/f234.php';
+    } elseif ($page == 'editmeja') {
+        require 'pelayan/f235.php';
+    }elseif ($page == 'hapusmeja') {
+        require 'pelayan/hapusmeja.php';
+    } elseif ($page == 'tambahpemesanan') {
+        require 'pelayan/f326.php';
+    } elseif ($page == 'editpemesanan') {
+        require 'pelayan/f237.php';
+    
+    
+    
+    
     } elseif ($page == 'logout') {
         require 'logout.php';
     } else {
