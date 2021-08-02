1279f731cf48872c8dc5816e6a7d99676313c4ab
resaend <resaend13@gmail.com>
2021-08-02 16:34:53 +0700

    validasi login

--- errorinfo.php
+++ errorinfo.php
@@ -1,14 +1,13 @@
 <?php
 error_reporting(0);
-if($_GET['error']==1){
-    echo '<h6 class="card-info text-center mb-5 my-3">mohon maaf, sedang terjadi masalah server</h6>';
-}elseif($_GET['error']==2){
-    echo '<h6 class="card-info text-center mb-5 my-3">mohon maaf, tidak dapat memasuki halaman</h6>';
-}elseif($_GET['error']==3 || $_GET['error']==4){
-    echo '<h6 class="card-info text-center mb-5 my-3">mohon maaf, username atau password tidak dapat ditemukan</h6>';
-}elseif($_GET['error']==5){
-    echo '<h6 class="card-info text-center mb-5 my-3">mohon lengkapi pengisian form tersebut agar data dapat dikirimkan</h6>';
-}elseif($_GET['error']==6){
-    echo '<h6 class="card-info text-center mb-5 my-3">mohon maaf, sedang terjadi masalah server ketika melakukan pengiriman data</h6>';
+if ($_GET['error'] == 1) {
+    echo '<div class="bg-warning px-2 py-2 rounded text-center mb-3 my-2">Mohon maaf, sedang terjadi masalah server</div>';
+} elseif ($_GET['error'] == 2) {
+    echo '<div class="bg-warning px-2 py-2 rounded text-center mb-3 my-2">Mohon maaf, tidak dapat memasuki halaman</div>';
+} elseif ($_GET['error'] == 3 || $_GET['error'] == 4) {
+    echo '<div class="bg-warning px-2 py-2 rounded text-center mb-3 my-2">Mohon maaf, username atau password tidak dapat ditemukan</div>';
+} elseif ($_GET['error'] == 5) {
+    echo '<div class="bg-warning px-2 py-2 rounded text-center mb-3 my-2">Mohon lengkapi pengisian form tersebut agar data dapat dikirimkan</div>';
+} elseif ($_GET['error'] == 6) {
+    echo '<div class="bg-warning px-2 py-2 rounded text-center mb-3 my-2">Mohon maaf, sedang terjadi masalah server ketika melakukan pengiriman data</div>';
 }
-?>
