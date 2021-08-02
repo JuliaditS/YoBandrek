bcc493cfb025e1e1e5a8830ed454a83ce5c54087
Ade Kurniawan <ade.kurniawan216@gmail.com>
2021-08-02 23:21:47 +0700

    new edit

--- includes/pelayan__navbar.php
+++ includes/pelayan__navbar.php
@@ -24,7 +24,26 @@ if ($_SESSION["level"]!="pelayan") {
                     <a class="nav-link" href="?page=pemesanan">Pemesanan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="?page=logout">Logout</a>
+                    <a class="nav-link btn" data-bs-toggle="modal" data-bs-target="#konfirmasi">Logout</a>
+
+                    <div class="modal fade" id="konfirmasi" tabindex="-1" aria-labelledby="konfirmasiModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="konfirmasiModalLabel"><i class="fas fa-question-circle fa-sm"></i> Konfirmasi</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Apakah anda yakin akan logout?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary"><a href="?page=logout" class="text-light text-decoration-none" >Ya</a></button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
