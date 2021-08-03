4746a98110d6b603ec4e35a659325ad21323328e
JuliaditS <juliaditsyahputra@gmail.com>
2021-08-03 23:43:18 +0700

    new Meja

--- pelayan/f233.php
+++ pelayan/f233.php
@@ -1,13 +1,15 @@
 <?php include 'includes/header.html' ?>
 <?php include 'includes/pelayan__navbar.php' ?>
-
+<form action="" method="POST">
 <div class="container mt-3">
     <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
         <div class="btn-group me-2" role="group" aria-label="First group" id="tambah" style="display: none;">
-            <a href="?page=tambahpemesanan" type="button" class="btn btnnew__medium"> <i class='bx bx-plus-circle'> </i>Tambah</a>
+            <input type="submit" name="tambah" class="btn btnnew__medium" value="Tambah">
+            <i class='bx bx-plus-circle'> </i>
         </div>
         <div class="btn-group mr-2" role="group" aria-label="Second group" id="edit" style="display: none;">
-            <a href="?page=editpemesanan" type="button" class="btn btn-warning"><i class='bx bx-edit'></i> Edit Pemesanan</a>
+            <input type="submit" name="edit" class="btn btn-warning" value="Edit Pemesanan">
+            <i class='bx bx-edit'></i>
         </div>
     </div>
 </div>
@@ -18,6 +20,7 @@
     </div>
 
 </div>
+</form>
 <script>
     $(document).ready(function() {
         setInterval(function() {
@@ -36,24 +39,56 @@
         }
     })
     function myfunction(ini){
-        var test =document.getElementsByName("nomeja");
-        var i;
-        for (i = 1; i < test.length; i++) {
-            
-            if (test[i].id==ini.id) {
-                test.checked = true;
-                console.log(test[i].id);
+        if (ini.checked==true) {
+            for (var i = 1; i <= document.getElementById('no').value; i++) {
+                if (document.getElementById(i).value == ini.value) {
+                    document.getElementById(i).checked=true;
+                    if (document.getElementById('status'+ini.value).value=="tersedia") {
+                        document.getElementById('tambah').style.display="block";
+                        document.getElementById('edit').style.display="none";
+                        for (var b = 1; b <= document.getElementById('no').value; b++) {
+                            if (document.getElementById(b).value!="") {
+                                document.getElementById(b).checked=false;
+                            } 
+                        }
+                    } else {
+                        document.getElementById('tambah').style.display="none";
+                        document.getElementById('edit').style.display="block";
+                        
+                    }
+                } else {
+                    if (document.getElementById('status'+ini.value).value=="tersedia") {
+                        document.getElementById('tambah').style.display="block";
+                        document.getElementById('edit').style.display="none";
+                        for (var b = 1; b <= document.getElementById('no').value; b++) {
+                            if (document.getElementById(b).value!="") {
+                                document.getElementById(b).checked=false;
+                            } 
+                        }
+                    } else {
+                        document.getElementById(i).checked=false;
+                    }
+                    
+                }
             }
-
-        }
-        // console.log(i);
-        if (document.getElementById('status'+ini.id).value=="tersedia") {
-            document.getElementById('tambah').style.display="block";
-            document.getElementById('edit').style.display="none";
+            
+            
         } else {
-            document.getElementById('tambah').style.display="none";
-            document.getElementById('edit').style.display="block";
+            for (var i = 1; i <= document.getElementById('no').value; i++) {
+                if (document.getElementById(i).value == ini.value) {
+                    document.getElementById(i).checked=false;
+                    document.getElementById('tambah').style.display="none";
+                    document.getElementById('edit').style.display="none";
+                }
+            }
         }
+        
+        // for (i = 0; i < ini.length; i++) {
+        //     document.getElementById(ini.id).checked = true;
+
+        // }
+        // console.log('status'+ini.value);
+        
     }
 </script>
 <?php include 'includes/footer.html' ?>
--- pelayan/loadmeja.php
+++ pelayan/loadmeja.php
@@ -2,10 +2,11 @@
 error_reporting(0);
 include "../includes/functions.php";
 $meja = mysqli_query($conn, "SELECT * FROM `data_meja` ORDER BY `data_meja`.`no_meja` ASC");
+$meja1 = mysqli_query($conn, "SELECT * FROM `data_meja` ORDER BY `data_meja`.`no_meja` ASC");
 $daftar= array();
 function acak($panjang)
 {
-    $karakter= 'ABCDEFabcdef1234567890';
+    $karakter= 'ABCDEabcde123456789';
     $string = '';
     for ($i = 0; $i < $panjang; $i++) {
     $pos = rand(0, strlen($karakter)-1);
@@ -13,11 +14,10 @@ function acak($panjang)
     }
     return $string;
 }
-while($data2 = mysqli_fetch_array($meja)){
+while($data2 = mysqli_fetch_array($meja1)){
     $daftar[$data2['no_pemesanan']][]=acak(6);
 }
-var_dump($daftar);
-die();
+$no=1;
 while($data = mysqli_fetch_array($meja)){
 ?>
         
@@ -27,20 +27,21 @@ while($data = mysqli_fetch_array($meja)){
                 <p style="color: black; text-decoration: none">
                 <div class="card text-black bg-white">
             <?php } else{ ?>
-                <div style="background-color: #1db133;"><p>test</p></div>
+                <p style="color: white; text-decoration: none">
+                <div class="card text-white" style="background-color: #<?php echo $daftar[$data['no_pemesanan']][0]; ?>;">
                 <?php
                 
 
                 $nomejapemesanan= $data['no_meja']." ".$data['no_pemesanan'];
                 $angka = substr($data['no_pemesanan'], -1);
                 
-                for ($i=0; $i <= sizeof($daftar); $i++) { 
-                    if ($daftar[$i]!=$data['no_pemesanan']) {
-                        if ($angka==substr($daftar[$i], -1)) {
-                            $angka++;
-                        }
-                    }
-                }
+                // for ($i=0; $i <= sizeof($daftar); $i++) { 
+                //     if ($daftar[$i]!=$data['no_pemesanan']) {
+                //         if ($angka==substr($daftar[$i], -1)) {
+                //             $angka++;
+                //         }
+                //     }
+                // }
                 // array_push($nomejapemesanan); 
                 // for ($i=0; $i <sizeof($daftar) ; $i++) { 
                 //      if (substr($daftar[i], 3)!=substr($nomejapemesanan, 3)) {
@@ -49,62 +50,7 @@ while($data = mysqli_fetch_array($meja)){
                 //          }
                 //      }
                 //  } 
-                switch ($angka) {
-                    case '1':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-success">
-                        <?php
-                        break;
-                    case '2':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-primary">
-                        <?php
-                        break;
-                    case '3':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-secondary">
-                        <?php
-                        break;
-                    case '4':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-danger">
-                        <?php
-                        break;
-                    case '5':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-warning">
-                        <?php
-                        break;
-                    case '6':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-info">
-                        <?php
-                        break;
-                    case '7':
-                        ?>
-                        <p style="color: dark; text-decoration: none">
-                        <div class="card text-dark bg-light">
-                        <?php
-                        break;
-                    case '8':
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-dark">
-                        <?php
-                        break;
-                    default:
-                        ?>
-                        <p style="color: white; text-decoration: none">
-                        <div class="card text-white bg-mute">
-                        <?php
-                        break;
-                }
+                
 
                  ?>
                 
@@ -112,7 +58,7 @@ while($data = mysqli_fetch_array($meja)){
                 
                 <h3 class="my-5">    
                     <div class="form-check d-flex justify-content-center">
-                        <input class="form-check-input" type="checkbox" value="" name="nomeja" id="<?php echo $data['no_pemesanan'] ?>" onchange="myfunction(this)">
+                        <input class="form-check-input" type="checkbox" value="<?php echo $data['no_pemesanan'] ?>" name="nomeja[]" id="<?php echo $no; ?>" onchange="myfunction(this)">
                         <input type="hidden" id="status<?php echo $data['no_pemesanan'] ?>" value="<?php echo $data['status']; ?>">
                         <input type="hidden" id="nopemesanan<?php echo $data['no_pemesanan'] ?>" value="<?php echo $data['no_pemesanan'] ?>">
                         <label class="form-check-label" for="defaultCheck1">
@@ -125,4 +71,7 @@ while($data = mysqli_fetch_array($meja)){
             </div>
             </a>
         </div>
-<?php } ?>      
+        <?php $no++; ?>
+
+<?php } ?>   
+<input type="hidden" id="no" value="<?php echo $no; ?>">   
